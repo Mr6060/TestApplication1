e60c6aa4332fdbee7446bd8d1a4113023ea9bd9f
Davoleo <davoleo2001@gmail.com>
2018-02-16 17:11:19 +0100

    Media 3 numeri, V2
    Non va neanche se la preghi

---
+++ TestApplication1/Media3N.cpp
@@ -0,0 +1,50 @@
+/*
+Author: D36l
+Date: 06/02/18
+Version: 1.0
+Copyright - © - Davoleo - 2018
+*/
+
+#include "stdafx.h"
+#include <iostream>
+
+main()
+{
+	using namespace std;
+
+	//Var creation
+	int a = 0, b = 0, c = 0;
+
+	//Data Input [Metodo separato]
+	cout << "Inserire il primo numero: ";
+	cin >> a;
+
+	cout << "\nInserire il secondo numero ";
+	cin >> b;
+
+	cout << "\nInserire il terzo numero ";
+	cin >> c;
+
+	//Data Input [Metodo Single Line]
+	//cout << "Inserisci 3 numeri interi separati da spazio: ";
+	//cin >> a >> b >> c;
+
+	//Processing
+	int somma = 0;
+	somma = a + b + c;
+
+	//int media1 = somma / 3; //divisione intera [NO decimali]
+
+	//int media2 = (a + b + c) / 3; //divisione intera [NO decimali]
+
+	//double media3 = (a + b + c) / 3; //divisione intera [NO decimali]
+
+	double media4 = (a + b + c) / 3.0; //divisione DECIMALE
+
+									   //Data Output
+	cout << somma << endl;
+	//cout << "Media 1: " << media1 << endl;
+	//cout << "Media 2: " << media2 << endl;
+	//cout << "Media 3: " << media3 << endl;
+	cout << "Media 4: " << media4 << endl;
+}
